d5f758502504052a7adab4169264c5bf331318c2
Thomas Roman <thomasroman32@yahoo.com>
2019-04-15 01:32:50 -0500

    create react app commit changed the app.js file to display multiple todo items also changed the app.css file to style each to do list item and create green background

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './App.css';
+function Todo({todo, index}) {
+  return (<div className="todo">{todo.text}</div>);
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmi={handleSubmit}>
+    <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React', 
+      isCompleted: false
+
+    }, 
+    {
+      text: 'Meet friend for lunch', 
+      isCompleted: false
+
+    }, 
+    {
+      text: 'Build to do app', 
+      isCompleted: false
+
+    }, 
+
+  ])
+  return (
+    <div className="app"> 
+    <div className="todo-list">
+      {todos.map((todo, index)=> (
+        <Todo key={index} index={index} todo={todo} /> 
+      ))}
+
+
+    </div>
+    </div>
+  )
+}
+
+export default App;
