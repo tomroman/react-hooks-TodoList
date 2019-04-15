a1ddb5f4a5248678976c152ced38c889060e0fe9
Thomas Roman <thomasroman32@yahoo.com>
2019-04-15 01:46:31 -0500

    created addTodo function and placed TodoForm component that adds tasks to the list.

--- src/App.js
+++ src/App.js
@@ -15,8 +15,13 @@ function TodoForm({addTodo}) {
   }
 
   return (
-    <form onSubmi={handleSubmit}>
-    <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    <form onSubmit={handleSubmit}>
+    <input 
+       type="text" 
+       className="input" 
+       value={value} 
+       placeholder="Add Todo..."
+       onChange={e => setValue(e.target.value)} />
     </form>
   )
 
@@ -40,13 +45,19 @@ function App() {
 
     }, 
 
-  ])
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos)
+  }
   return (
     <div className="app"> 
     <div className="todo-list">
       {todos.map((todo, index)=> (
         <Todo key={index} index={index} todo={todo} /> 
       ))}
+      <TodoForm addTodo={addTodo} />
 
 
     </div>
