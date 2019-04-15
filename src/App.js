884199eb56c4e7fa492dfdb738bc8d65d9b96118
Thomas Roman <thomasroman32@yahoo.com>
2019-04-15 15:59:23 -0500

    created complete todo and remove todo functions along with new button to delete completed todo item in list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-function Todo({todo, index}) {
-  return (<div className="todo">{todo.text}</div>);
+
+function Todo({todo, index, completeTodo, removeTodo}) {
+  return (<div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} 
+  className="todo">
+  {todo.text}
+  <div>
+    <button onClick={() => completeTodo(index)}>Complete</button>
+    <button onClick={() => removeTodo(index)}>X</button>
+  </div>
+  </div>);
 }
 
 function TodoForm({addTodo}) {
@@ -50,12 +58,34 @@ function App() {
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos)
+  };
+  
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+
+
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
   }
+
+
   return (
     <div className="app"> 
     <div className="todo-list">
       {todos.map((todo, index)=> (
-        <Todo key={index} index={index} todo={todo} /> 
+        <Todo key={index} 
+              index={index} 
+              todo={todo} 
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+              
+              /> 
       ))}
       <TodoForm addTodo={addTodo} />
 
